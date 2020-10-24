49365dc09f84d07734d46d3e19a4a82fa563edb7
Anxious <53601508+anxious4glory@users.noreply.github.com>
2020-10-24 12:27:20 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php
+
+    if (isset($_POST['submit'])) {
+        
+        $from = $_POST['email'];
+        $to = 'contact.cybermaven@gmail.com';
+        $subject = 'Email signup';
+        $body = 'Please sign me up to the mailing list';
+        
+        if (!$_POST['email']) {
+            $emailError = '<div class="text-danger"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;Please enter a valid email address</div>';
+        }
+        
+        if (!$emailError) {
+            if (mail ($to, $subject, $body, $from)) {
+                $result = '<div class="text-success"><i class="fa fa-check" aria-hidden="true"></i> &nbsp;thank you we\'ll keep you updated</div>';
+            } else {
+                $result = '<div class="text-danger"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;sorry there has been an error, please try again</div>';
+            }
+        }
+        
+    }
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -72,7 +97,8 @@
       </div>
     </section>
 
-
+    <?php echo $emailError;?>
+    <?php echo $result;?>
 
 
     <!-- Optional JavaScript; choose one of the two! -->
