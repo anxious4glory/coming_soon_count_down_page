d45a1e20ccf697b6601420cc03957a8c19600b0b
Anxious <53601508+anxious4glory@users.noreply.github.com>
2020-10-24 12:37:55 +0100

    second update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
         $body = 'Please sign me up to the mailing list';
         
         if (!$_POST['email']) {
-            $emailError = '<div class="text-danger"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;Please enter a valid email address</div>';
+            $emailError = '<div class="text-danger"><i class="fas fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;Please enter a valid email address</div>';
         }
         
         if (!$emailError) {
             if (mail ($to, $subject, $body, $from)) {
-                $result = '<div class="text-success"><i class="fa fa-check" aria-hidden="true"></i> &nbsp;thank you we\'ll keep you updated</div>';
+                $result = '<div class="text-success"><i class="fas fa-check" aria-hidden="true"></i> &nbsp;thank you we\'ll keep you updated</div>';
             } else {
-                $result = '<div class="text-danger"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;sorry there has been an error, please try again</div>';
+                $result = '<div class="text-danger"><i class="fas fa-exclamation-triangle" aria-hidden="true"></i> &nbsp;sorry there has been an error, please try again</div>';
             }
         }
         
